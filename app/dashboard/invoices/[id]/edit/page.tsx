551010dfef95d081d3c3ded740c83cb057a5afe7
José Guerrero <jose.guerrero.carrizo@gmail.com>
2023-11-05 10:14:44 -0500

    fix commit second

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,11 +1,12 @@
 import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+import { InvoiceForm } from "@/app/lib/definitions";
 
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
   const [invoice, customers] = await Promise.all([
-    fetchInvoiceById(id),
+    fetchInvoiceById(id) as Promise<InvoiceForm>,
     fetchCustomers(),
   ]);
   return (
